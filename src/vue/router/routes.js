55fe03376765fb25981956bbb7737710828171c8
Luiz Kim <luiz.kim@controleonline.com>
2025-03-11 16:30:45 -0300

    Update

--- src/vue/router/routes.js
+++ src/vue/router/routes.js
@@ -1,7 +1,7 @@
 export const routes = [
   {
     path: '/professionals/',
-    component: () =>  import ('@controleonline/ui-layout/src/layouts/AdminLayout.vue'),
+    component: () =>  import ('@controleonline/ui-layout/src/vue/layouts/AdminLayout.vue'),
     children: [
       {
         name: 'ProfessionalsIndex',
